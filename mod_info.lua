75e860fabced41cbeebc8093452d58158ab02977
speed2CZ <GTGTGT@seznam.cz>
2017-04-04 10:49:14 +0200

    Add mod_info.lua

---
+++ mod_info.lua
@@ -0,0 +1,21 @@
+-- Forged Alliance Forever coop mod_info.lua file
+--
+-- Documentation for the extended FAF mod_info.lua format can be found here:
+-- https://github.com/FAForever/fa/wiki/mod_info.lua-documentation
+name = "FAF Galactic War support mod"
+version = 1
+_faf_modname='galactic_war'
+copyright = "Forged Alliance Forever Community"
+description = "Support mod for Galactic War"
+author = "Forged Alliance Forever Community"
+url = "http://www.faforever.com"
+uid = "b2c57d9c-e41e-4bee-9a00-b2596efaa680"
+selectable = false
+exclusive = false
+ui_only = false
+conflicts = {}
+mountpoints = {
+    lua = '/lua',
+    units = '/units',
+    'mods/galacticWar/hook' = '/schook'
+}
