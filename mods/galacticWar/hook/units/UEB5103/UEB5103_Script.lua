9706b917d087f116ef0f588cfcee57045f6b5c6c
speed2CZ <GTGTGT@seznam.cz>
2017-09-29 21:38:14 +0200

    Reduce the size of the flashing lights

--- mods/galacticWar/hook/units/UEB5103/UEB5103_Script.lua
+++ mods/galacticWar/hook/units/UEB5103/UEB5103_Script.lua
@@ -12,12 +12,12 @@ local TStructureUnit = import('/lua/terranunits.lua').TStructureUnit
 
 UEB5103 = Class(TStructureUnit) {
     FxTransportBeacon = {'/effects/emitters/red_beacon_light_01_emit.bp'},
-    FxTransportBeaconScale =1,
+    FxTransportBeaconScale = 0.4,
 
     OnStopBeingBuilt = function(self)
         TStructureUnit.OnStopBeingBuilt(self)
         for k, v in self.FxTransportBeacon do
-            self.Trash:Add(CreateAttachedEmitter(self, 0, -1, v):ScaleEmitter(self.FxTransportBeaconScale))
+            self.Trash:Add(CreateAttachedEmitter(self, 0, self:GetArmy(), v):ScaleEmitter(self.FxTransportBeaconScale))
         end
     end,
 }
--- mods/galacticWar/hook/units/URB5103/URB5103_Script.lua
+++ mods/galacticWar/hook/units/URB5103/URB5103_Script.lua
@@ -12,12 +12,12 @@ local CStructureUnit = import('/lua/cybranunits.lua').CStructureUnit
 
 URB5103 = Class(CStructureUnit) {
     FxTransportBeacon = {'/effects/emitters/red_beacon_light_01_emit.bp'},
-    FxTransportBeaconScale =1,
+    FxTransportBeaconScale = 0.4,
 
     OnStopBeingBuilt = function(self)
         CStructureUnit.OnStopBeingBuilt(self)
         for k, v in self.FxTransportBeacon do
-            self.Trash:Add(CreateAttachedEmitter(self, 0,self:GetArmy(), v):ScaleEmitter(self.FxTransportBeaconScale))
+            self.Trash:Add(CreateAttachedEmitter(self, 0, self:GetArmy(), v):ScaleEmitter(self.FxTransportBeaconScale))
         end
     end,
 }
