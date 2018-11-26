ff435a3b960729d378a6268e4d15ae790d64dfd5
speed2 <GTGTGT@seznam.cz>
2018-11-26 13:35:45 +0100

    Update OnCreateAI
    Prolly want to figure out a hook for the future....

--- mods/galacticWar/hook/lua/aibrain.lua
+++ mods/galacticWar/hook/lua/aibrain.lua
@@ -40,17 +40,27 @@ AIBrain = Class(oldAIBrain) {
             -- Flag this brain as a possible brain to have skirmish systems enabled on
             self.SkirmishSystems = true
 
-            local cheatPos = string.find( per, 'cheat')
+            local cheatPos = string.find(per, 'cheat')
             if cheatPos then
                 AIUtils.SetupCheat(self, true)
-                ScenarioInfo.ArmySetup[self.Name].AIPersonality = string.sub( per, 1, cheatPos - 1 )
+                ScenarioInfo.ArmySetup[self.Name].AIPersonality = string.sub(per, 1, cheatPos - 1)
             end
 
+            LOG('* OnCreateAI: AIPersonality: ('..per..')')
+            if string.find(per, 'sorian') then
+                self.Sorian = true
+            end
+            if string.find(per, 'uveso') then
+                self.Uveso = true
+            end
+            if string.find(per, 'dilli') then
+                self.Dilli = true
+            end
+            if DiskGetFileInfo('/lua/AI/altaiutilities.lua') then
+                self.Duncan = true
+            end
 
             self.CurrentPlan = self.AIPlansList[self:GetFactionIndex()][1]
-
-            --LOG('*AI DEBUG: AI PLAN LIST = ', repr(self.AIPlansList))
-            --LOG('===== AI DEBUG: AI Brain Fork Theads =====')
             self.EvaluateThread = self:ForkThread(self.EvaluateAIThread)
             self.ExecuteThread = self:ForkThread(self.ExecuteAIThread)
 
@@ -63,17 +73,19 @@ AIBrain = Class(oldAIBrain) {
                 ScoutCounter = 0,
             }
 
-            ------changed this for Sorian AI
-            --Flag enemy starting locations with threat?
-            if ScenarioInfo.type == 'skirmish' and string.find(per, 'sorian') then
-                --Gives the initial threat a type so initial land platoons will actually attack it.
-                self:AddInitialEnemyThreatSorian(200, 0.005, 'Economy')
-            elseif ScenarioInfo.type == 'skirmish' then
-                self:AddInitialEnemyThreat(200, 0.005)
+            -- Flag enemy starting locations with threat?
+            if ScenarioInfo.type == 'skirmish' then
+                if self.Sorian then
+                    -- Gives the initial threat a type so initial land platoons will actually attack it.
+                    self:AddInitialEnemyThreatSorian(200, 0.005, 'Economy')
+                else
+                    self:AddInitialEnemyThreat(200, 0.005)
+                end
             end
         end
         self.UnitBuiltTriggerList = {}
         self.FactoryAssistList = {}
+        self.DelayEqualBuildPlattons = {}
         self.BrainType = 'AI'
     end,
 
