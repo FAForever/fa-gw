5f08ba09e152468380714bae106e4a60906b405a
speed2CZ <odstrcilpetr@seznam.cz>
2025-10-19 21:22:07 +0200

    Bump version

--- mod_info.lua
+++ mod_info.lua
@@ -3,13 +3,13 @@
 -- Documentation for the extended FAF mod_info.lua format can be found here:
 -- https://github.com/FAForever/fa/wiki/mod_info.lua-documentation
 name = "FAF Galactic War support mod"
-version = 1
-_faf_modname='galactic_war'
+version = 2
+_faf_modname ='galactic_war'
 copyright = "Forged Alliance Forever Community"
 description = "Support mod for Galactic War"
 author = "Forged Alliance Forever Community"
 url = "http://www.faforever.com"
-uid = "b2c57d9c-e41e-4bee-9a00-b2596efaa680"
+uid = "804f1e70-fe75-438f-a96f-7e423e4b8e2a"
 selectable = false
 exclusive = false
 ui_only = false
