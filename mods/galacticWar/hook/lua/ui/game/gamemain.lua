29751df4bc49188ec10b2f43d164b56903ec6669
Alexander <gastec139@gmail.com>
2025-12-16 11:35:13 +0300

    fix reset of timers on pause-unpause

--- mods/galacticWar/hook/lua/ui/game/gamemain.lua
+++ mods/galacticWar/hook/lua/ui/game/gamemain.lua
@@ -1,26 +1,7 @@
-local oldCreateUI = CreateUI
-function CreateUI(isReplay)
-    oldCreateUI(isReplay)
-    import('/lua/ui/ability_panel/abilities.lua').SetupOrdersControl(gameParent)
-end
-
-local oldOnPause = OnPause
-function OnPause(pausedBy, timeoutsRemaining)
-    oldOnPause(pausedBy, timeoutsRemaining)
-    import('/lua/ui/ability_panel/abilities.lua').KillTimers()
-end
-
-local oldOnResume = OnResume
-function OnResume()
-    oldOnResume()
-    import('/lua/ui/ability_panel/abilities.lua').RestartTimers()
-end
-
--- Removing live replay syncing
-function UiBeat()
-    local observing = (GetFocusArmy() == -1)
-    if (observing ~= lastObserving) then
-        lastObserving = observing
-        import('/lua/ui/game/economy.lua').ToggleEconPanel(not observing)
+do
+    local oldCreateUI = CreateUI
+    function CreateUI(isReplay)
+        oldCreateUI(isReplay)
+        import('/lua/ui/ability_panel/abilities.lua').SetupOrdersControl(gameParent)
     end
 end
