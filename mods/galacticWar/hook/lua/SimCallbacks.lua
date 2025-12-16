b776ddaa7605c861d1174f77707ae8bda3e4c9d4
Alexander <gastec139@gmail.com>
2025-12-16 12:42:23 +0300

    remove redundant callbacks

--- mods/galacticWar/hook/lua/SimCallbacks.lua
+++ mods/galacticWar/hook/lua/SimCallbacks.lua
@@ -28,30 +28,4 @@ do
 			end
 		end
 	end
-
-	-- issue:#43
-	Callbacks.AddTarget = function(data, units)
-		if type(data.target) == 'string' then
-			local entity = GetEntityById(data.target)
-			if entity and IsBlip(entity) and entity.GetSource then
-				local blipentity = entity:GetSource()
-				if blipentity then
-					if IsUnit(blipentity) then
-						for id, unit in units or {} do
-							blipentity:addAttacker(unit)
-							unit:addTarget(blipentity)
-						end
-					end
-				end
-			end
-		end
-	end
-
-	Callbacks.ClearTargets = function(data, units)
-		for id, unit in units or {} do
-			if unit then
-				unit:clearTarget()
-			end
-		end
-	end
 end
